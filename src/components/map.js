b8afc61309c2b4c91b5a44937f8f87a8e51d34a3
arnaud-lavit <arnaud.lavit@outlook.com>
2019-08-22 16:01:51 +0200

    add react-motion for map

--- src/components/map.js
+++ src/components/map.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { push } from "gatsby"
+import { Link } from "gatsby"
 import {
   ComposableMap,
   ZoomableGroup,
@@ -7,132 +7,173 @@ import {
   Geography,
   Markers,
   Marker,
-  Annotations,
   Annotation
 } from "react-simple-maps"
+import { Motion, spring } from "react-motion"
 
+import mapStyles from './map.module.scss'
 import worldJson from "../../static/world-50m-simplified.json"
 
+
 class ZoomPan extends Component {
-   //handleMapClick(geography) {
-   //  const { cities } = this.props
-   //  // TODO Temporary fix. Will need to create a mapping later
-   //  const country =
-   //    geography.properties.NAME === `United States of America`
-   //      ? `Usa`
-   //      : geography.properties.NAME
-   //  const destination = cities.filter(city => city.country === country)
-   //  console.log(country)
-   //  push(`/blog/london`)
-   //  if (destination.length > 0) {
-   //    
-   //    push(`/blog/london`)
-   //  }
-   //}
+
+  state = {
+    center: [0,20],
+    zoom: 1,
+    annotation: false,
+    city: null
+  }
+
+  handleCityClick = city => {
+    this.setState({
+      zoom: 2,
+      center: [city.coordinates.lon, city.coordinates.lat],
+      annotation: true,
+      city: city
+    })
+  }
+
+  handleCityButtonClick = city => {
+    this.setState({
+      zoom: 2,
+      center: [city.coordinates.lon, city.coordinates.lat],
+      annotation: true,
+      city: city
+    })
+  }
+
+  handleReset = () => {
+    this.setState({
+      center: [0,20],
+      zoom: 1,
+      annotation: false
+    })
+  }
 
   render() {
-    const { cities, center, zoom } = this.props
+    const { cities } = this.props
+    const { center, zoom, annotation, city } = this.state
 
     return (
-      <div>
-        <ComposableMap
-          projectionConfig={{
-            scale: 500,
-          }}
-          width={1200}
-          height={zoom > 1 ? 500 : 900}
-          style={{
-            width: `100%`,
-            height: `auto`,
-          }}
-        >
-          <ZoomableGroup center={center} zoom={zoom} disablePanning>
-            <Geographies geography={worldJson}>
-              {(geographies, projection) =>
-                geographies.map(
-                  geography =>
-                    geography.id !== `ATA` && (
-                      <Geography
-                        key={geography.properties.NAME}
-                        geography={geography}
-                        projection={projection}
-                        //onClick={() => this.handleMapClick(geography)}
-                        style={{
-                          default: {
-                            fill: `#f0f0f0`,
-                            stroke: `#cdcdcd`,
-                            strokeWidth: 0.75,
-                            outline: `none`,
-                          },
-                          hover: {
-                            fill: `#828282`,
-                            stroke: `#cdcdcd`,
-                            strokeWidth: 0.75,
-                            outline: `none`,
-                          },
-                        }}
+      <div className={mapStyles.container}>
+        <div className={mapStyles.cities}>
+          <ul>
+          {cities.map((marker, id) => (      
+            <li key={id}>
+            <button onClick={() => this.handleCityButtonClick(marker)}>
+            { marker.city }
+            </button>
+            </li>
+          ))}
+          <li><button className={mapStyles.resetbtn} onClick={this.handleReset}>
+            { "Reset" }
+          </button>
+          </li>
+          </ul>
+          <div>
+          
+          </div>
+        </div>
+        <div className={mapStyles.map}>
+          <Motion
+            defaultStyle={{
+              zoom: 1,
+              x: 0,
+              y: 20,
+            }}
+            style={{
+              zoom: spring(zoom, {stiffness: 210, damping: 20}),
+              x: spring(center[0], {stiffness: 210, damping: 20}),
+              y: spring(center[1], {stiffness: 210, damping: 20}),
+            }}
+          >
+          {({zoom,x,y}) => (
+            <ComposableMap
+              projectionConfig={{
+                scale: 300,
+              }}
+              width={800}
+              height={551}
+              style={{
+                width: `100%`,
+                height: `auto`,
+              }}
+            >
+              <ZoomableGroup center={[x,y]} zoom={zoom}>
+              <Geographies geography={worldJson}>
+                {(geographies, projection) =>
+                  geographies.map(
+                    geography =>
+                      geography.id !== `ATA` && (
+                        <Geography
+                          key={geography.properties.NAME}
+                          geography={geography}
+                          projection={projection}
+                          //onClick={() => this.handleMapClick(geography)}
+                          style={{
+                            default: {
+                              fill: `#f0f0f0`,
+                              stroke: `#cdcdcd`,
+                              strokeWidth: 0.75,
+                              outline: `none`,
+                            },
+                            hover: {
+                              fill: `#828282`,
+                              stroke: `#cdcdcd`,
+                              strokeWidth: 0.75,
+                              outline: `none`,
+                            },
+                          }}
+                        />
+                      )
+                  )
+                }
+              </Geographies>
+                <Markers>
+                  {cities.map((marker, id) => (
+                    <Marker
+                      key={id}
+                      marker={{ coordinates: [marker.coordinates.lon, marker.coordinates.lat] }}
+                      onClick={() => this.handleCityButtonClick(marker)}
+                      style={{
+                        default: { stroke: `#505050` },
+                      }}
+                    >
+                      <circle
+                        cx={0}
+                        cy={0}
+                        r={5}
+                        fill="#FF5722"
+                        stroke="#DF3702"
                       />
-                    )
-                )
-              }
-            </Geographies>
-            <Markers>
-              {cities.map((marker, id) => (
-                <Marker
-                  key={id}
-                  marker={{ coordinates: [marker.coordinates.lon, marker.coordinates.lat] }}
-                  style={{
-                    default: { stroke: `#505050` },
-                  }}
-                >
-                  <g transform="translate(-12, -24)">
-                    <path
-                      fill="none"
-                      strokeWidth="3"
-                      strokeLinecap="square"
-                      strokeMiterlimit="10"
-                      strokeLinejoin="miter"
-                      d="M20,9c0,4.9-8,13-8,13S4,13.9,4,9c0-5.1,4.1-8,8-8S20,3.9,20,9z"
-                    />
-                    <circle
-                      fill="none"
-                      strokeWidth="2"
-                      strokeLinecap="square"
-                      strokeMiterlimit="10"
-                      strokeLinejoin="miter"
-                      cx="12"
-                      cy="9"
-                      r="3"
-                    />
-                  </g>
-                </Marker>
-              ))}
-            </Markers>
-            <Annotations>
-              {cities.map((marker, id) => (
-                <Annotation
-                  key={id}
-                  dx={ -90 }
-                  dy={ -30 }
-                  subject={ [ marker.coordinates.lon, marker.coordinates.lat ] }
-                  strokeWidth={ 1 }
-                  curve={0.5}
-                  stroke="#607D8B"
-                  >
-                  <text>{ marker.city }</text>
-                </Annotation>
-              ))}
-            </Annotations>
-          </ZoomableGroup>
-        </ComposableMap>
+                    </Marker>
+                  ))}
+                </Markers>
+                  {annotation ? (
+                    <Annotation
+                      dx={ -90 }
+                      dy={ -30 }
+                      subject={ [ city.coordinates.lon, city.coordinates.lat ] }
+                      strokeWidth={ 1 }
+                      curve={0.5}
+                      stroke="#607D8B"
+                      >
+                      <Link to={`/blog/${city.city}`}><text>{ city.city }</text></Link>
+                    </Annotation>
+                  ): null
+                  }
+                  
+              </ZoomableGroup>
+            </ComposableMap>
+          )}
+          </Motion>
+        </div>
       </div>
     )
   }
 }
 
 ZoomPan.defaultProps = {
-  center: [0, 20],
-  zoom: 1,
   cities: [],
 }
 
--- src/pages/destinations.js
+++ src/pages/destinations.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
-import Map from "../components/map"
+import Map from '../components/map'
+import destinationsStyles from './destinations.module.scss'
 
 const DestinationsPage = () => {
   const data = useStaticQuery(graphql`
@@ -10,7 +11,7 @@ const DestinationsPage = () => {
       allContentfulBlogPost {
         edges {
           node {
-            title
+            slug
             location {
               lon
               lat
@@ -24,13 +25,13 @@ const DestinationsPage = () => {
   const coords = data.allContentfulBlogPost.edges.map(edge => {
     return {
       coordinates: edge.node.location,
-      city: edge.node.title
+      city: edge.node.slug
     }
   })
   return (
     <Layout>
       <Head title="Destinations" />
-      <Map cities={coords}/>
+      <Map className={destinationsStyles.map} cities={coords}/>
     </Layout>
   )
 }
